e7acbbe0bb849947f8f9ab47c40b039b5178b647
Harshil Bansal <harshilbansal27@gmail.com>
2022-06-04 15:51:07 +0530

    Uploaded uncheck code

---
+++ uncheck.py
@@ -0,0 +1,14 @@
+def add(x, y):
+    print("Adding...uncheck")
+    z = x+y
+    return z
+
+
+def sub(x, y):
+    print('Subtracting...')
+    z = x - y
+    return z
+
+
+x = 90
+d = {'Name':"Add", 'Module':1}
